aac9aaa23551ac7689fc76c930cb0b4b9591106a
Douglas <dev.douglasf@gmail.com>
2024-07-23 16:34:45 -0300

    funcao_da_divisao
    adicionada a funcao da divisao ao codigo

--- obj/Debug/net8.0/calculator.AssemblyInfo.cs
+++ obj/Debug/net8.0/calculator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("calculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0031135db76311988a700b171e535d90fdb663a")]
 [assembly: System.Reflection.AssemblyProductAttribute("calculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("calculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
