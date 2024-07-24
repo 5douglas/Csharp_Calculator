39029d58bb87e969f881a6e0091acb233c8c72d3
Douglas <dev.douglasf@gmail.com>
2024-07-24 13:17:24 -0300

    menu_adicionado
    menu de interação com usuario adicionado e uma simples laço de repetição criado

--- Program.cs
+++ Program.cs
@@ -6,9 +6,35 @@ namespace Calculator
     {
         static void Main(String [] args)
         {
-            Multiplicacao();
+            Menu();
         }
 
+        static void Menu()
+        {
+            Console.Clear();
+
+            Console.WriteLine("Qual operação você deseja realizar?");
+            Console.WriteLine("1 - Adição");
+            Console.WriteLine("2 - Subtração");
+            Console.WriteLine("3 - Divisão");
+            Console.WriteLine("4 - Multiplicação");
+            Console.WriteLine("5 - Sair");
+
+            Console.WriteLine("-------------------------------------");
+            Console.WriteLine("Selecione uma opção: ");
+
+            short escolha = short.Parse(Console.ReadLine());
+
+            switch(escolha)
+            {
+                case 1: Soma(); break;
+                case 2: Subtracao(); break;
+                case 3: Divisao(); break;
+                case 4: Multiplicacao(); break;
+                case 5: System.Environment.Exit(0); break;
+                default: Menu(); break;
+            }
+        }
         static void Soma()
 
         {
@@ -22,7 +48,10 @@ namespace Calculator
 
             float resultado = v1 + v2;
             Console.WriteLine("O resultado da soma é: " + resultado);
+
+            Console.WriteLine("\nAperte qualquer tecla para continuar");
             Console.ReadKey();
+            Menu();
         }
 
         static void Subtracao()
@@ -39,7 +68,10 @@ namespace Calculator
 
             float resultado = v1 - v2;
             Console.WriteLine($"O resultado da subtração é: {resultado}");
+
+            Console.WriteLine("\nAperte qualquer tecla para continuar");
             Console.ReadKey();
+            Menu();
         }
 
         static void Divisao()
@@ -56,7 +88,10 @@ namespace Calculator
 
             float resultado = v1/v2;
             Console.WriteLine($"O resultado da divisão é: {resultado}");
+
+            Console.WriteLine("\nAperte qualquer tecla para continuar");
             Console.ReadKey();
+            Menu();
         }
 
         static void Multiplicacao()
@@ -70,10 +105,13 @@ namespace Calculator
             float v2 = float.Parse(Console.ReadLine());
 
             Console.WriteLine("");
-            
+
             float resultado = v1*v2;
             Console.WriteLine($"O resultado da multiplicação é: {resultado}");
+
+            Console.WriteLine("\nAperte qualquer tecla para continuar");
             Console.ReadKey();
+            Menu();
         }
 
     }
